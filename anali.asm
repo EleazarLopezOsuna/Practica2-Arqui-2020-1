34076a7582b7e3a72099598a95c06407826807db
JaredOsuna <48371383+JaredOsuna@users.noreply.github.com>
2020-03-31 17:47:25 -0600

    Create anali.asm

---
+++ anali.asm
@@ -0,0 +1,346 @@
+fileReader MACRO
+   pushear
+   MOV AH, 3DH ; abre el archivo
+      MOV AL, 0 ; abre para lectura
+      LEA DX, calculadoraNombreArchivo
+      INT 21H ; interrupcion
+      MOV [manejadorArchivoCalculadora], AX ; carga el handler
+
+      MOV AH, 3FH ; leer
+      LEA DX, calculadoraTextoRecibido ; se almacena en texto
+      MOV CX, 1000H ; lee 100 caracteres
+      MOV BX, [manejadorArchivoCalculadora] ; recorre buffer
+      INT 21H ; interrupcion
+      MOV BX, [manejadorArchivoCalculadora]
+      MOV AH, 3EH ; cierra el archivo
+      INT 21H  ; interrupcion
+   poppear
+ENDM
+
+operar MACRO
+   LOCAL ECAR, CICLO, SALIR, ASIG, MULT, DIVI, CONTINUAR, VSIG
+   pushear
+   XOR BX, BX
+   ;pilaOperaciones
+   CICLO:
+      XOR AX, AX
+      ;===================Verificacion si las decenas son digito
+         CMP calculadoraTextoRecibido[BX], 0030H
+         JB VSIG
+         CMP calculadoraTextoRecibido[BX], 0039H
+         JA VSIG
+
+      ;===================Verificacion si las unidades son digito
+         CMP calculadoraTextoRecibido[BX + 1], 0030H
+         JB VSIG
+         CMP calculadoraTextoRecibido[BX + 1], 0039H
+         JA VSIG
+
+      ;TRANSFORMAR NUMERO Y ALMACENAR EN LA PILA
+         MOV AL, calculadoraTextoRecibido[BX] ;Digito mas significativo
+         MOV AH, calculadoraTextoRecibido[BX + 1] ;Digito menos significativo
+         transformarNumeroAlmacenarPila
+         ADD BX, 0003
+
+      ;===================Verificacion si el caracter es un signo valido
+      ;-------------------002A -> *
+      ;-------------------002B -> +
+      ;-------------------002D -> -
+      ;-------------------002F -> /
+      VSIG:
+         CMP calculadoraTextoRecibido[BX], 002BH ; Es suma
+         JE ASIG
+         CMP calculadoraTextoRecibido[BX], 002DH ; Es resta
+         JE ASIG
+         CMP calculadoraTextoRecibido[BX], 002FH ; Es division
+         JE DIVI
+         CMP calculadoraTextoRecibido[BX], 002AH ; Es multiplicacion
+         JE MULT
+         JMP ECAR
+
+      ASIG:
+         ;AGREGAR EL OPERADOR A LA LISTA
+         MOV AL, calculadoraTextoRecibido[BX]
+
+         push BX
+         XOR BX, BX
+         MOV BL, indexOperaciones
+         MOV AH, 0000
+         MOV pilaOperaciones[BX], AX
+         INC indexOperaciones
+         pop BX
+
+         ADD BX, 0002
+         JMP CONTINUAR
+
+      DIVI:
+         ADD BX, 0002 ;==Operador 2
+            ;===================Verificacion si las decenas son digito
+               CMP calculadoraTextoRecibido[BX], 0030H
+               JB ENUM
+               CMP calculadoraTextoRecibido[BX], 0039H
+               JA ENUM
+   
+            ;===================Verificacion si las unidades son digito
+               CMP calculadoraTextoRecibido[BX + 1], 0030H
+               JB ENUM
+               CMP calculadoraTextoRecibido[BX + 1], 0039H
+               JA ENUM
+
+         ;TRANSFORMAR NUMERO Y ALMACENAR EN BL
+            MOV AL, calculadoraTextoRecibido[BX] ;Digito mas significativo
+            MOV AH, calculadoraTextoRecibido[BX + 1] ;Digito menos significativo
+            transformarDi
+
+         ADD BX, 0003
+         JMP CONTINUAR
+
+      MULT:
+         ADD BX, 0002 ;==Operador 2
+            ;===================Verificacion si las decenas son digito
+               CMP calculadoraTextoRecibido[BX], 0030H
+               JB ENUM
+               CMP calculadoraTextoRecibido[BX], 0039H
+               JA ENUM
+   
+            ;===================Verificacion si las unidades son digito
+               CMP calculadoraTextoRecibido[BX + 1], 0030H
+               JB ENUM
+               CMP calculadoraTextoRecibido[BX + 1], 0039H
+               JA ENUM
+
+         ;TRANSFORMAR NUMERO Y ALMACENAR EN BL
+            MOV AL, calculadoraTextoRecibido[BX] ;Digito mas significativo
+            MOV AH, calculadoraTextoRecibido[BX + 1] ;Digito menos significativo
+            transformarMu
+
+         ADD BX, 0003
+         JMP CONTINUAR
+
+      CONTINUAR:
+         CMP calculadoraTextoRecibido[BX], 0024H ; Fin de cadena
+         JE ECAR
+         CMP calculadoraTextoRecibido[BX], 003BH ; Punto y coma
+         JE SALIR
+         JMP CICLO
+
+   ENUM:
+      ;MENSAJE DE ERROR
+      print errorIngresarFuncion
+      CALL PAUSA
+      JMP MENU
+
+   ECAR:
+      ;MENSAJE DE ERROR
+      print errorFaltaPuntoComa
+      CALL PAUSA
+      JMP MENU
+
+   SALIR:
+      poppear
+ENDM
+
+transformarDi MACRO
+   LOCAL ECER, SALIR
+   pushear
+   ;=============SI contiene el numero mas significativo
+   ;=============SI + 1 contiene el numero menos significativo
+   ;=============Los numeros se reciben en ascii. 0 -> 0030H, 1 -> 0031H, ... , 9 -> 0039H
+   SUB AL, 0030H ; Nos quedamos con el valor en hexa
+   SUB AH, 0030H ; Nos quedamos con el valor en hexa
+
+   MOV numeroMas, AL ; Guardamos los datos de los numeros
+   MOV numeroMenos, AH ; Guardamos los datos de los numeros
+
+   MOV BL, numeroMas
+   MOV AL, 0010 ; Para operar las decenas                AB          A = 5, B = 3
+   MUL BL ; Obtenemos las decenas                        A*10        5*10 = 50
+   ADD AL, numeroMenos ; Sumamos las unidades            A*10 + B    50 + 3 = 53
+
+   MOV BL, AL
+   MOV BH, 0000
+   ;OBTENER EL ULTIMO DATO INGRESADO EN LA PILA
+      obtenerUltimoPila
+      XOR AH, AH
+      XOR DX, DX
+      MOV AX, operador1
+
+   CMP BX, 0000
+   JE ECER
+
+   DIV BX
+   reemplazarUltimoPila
+   JMP SALIR
+
+   ECER:
+      ;MENSAJE DE ERROR
+      print errorDivisionCero
+      CALL PAUSA
+      JMP MENU
+
+   SALIR:
+      poppear
+ENDM
+
+transformarMu MACRO
+   pushear
+   ;=============SI contiene el numero mas significativo
+   ;=============SI + 1 contiene el numero menos significativo
+   ;=============Los numeros se reciben en ascii. 0 -> 0030H, 1 -> 0031H, ... , 9 -> 0039H
+   SUB AL, 0030H ; Nos quedamos con el valor en hexa
+   SUB AH, 0030H ; Nos quedamos con el valor en hexa
+
+   MOV numeroMas, AL ; Guardamos los datos de los numeros
+   MOV numeroMenos, AH ; Guardamos los datos de los numeros
+
+   MOV BL, numeroMas
+   MOV AL, 0010 ; Para operar las decenas                AB          A = 5, B = 3
+   MUL BL ; Obtenemos las decenas                        A*10        5*10 = 50
+   ADD AL, numeroMenos ; Sumamos las unidades            A*10 + B    50 + 3 = 53
+   XOR BH, BH
+   MOV BL, AL
+   ;OBTENER EL ULTIMO DATO INGRESADO EN LA PILA
+      obtenerUltimoPila
+      XOR AX, AX
+      MOV AX, operador1
+
+   MUL BX
+   reemplazarUltimoPila
+   poppear
+ENDM
+
+reemplazarUltimoPila MACRO
+   pushear
+   ;============AX contiene el resultado de la conversion de los numeros
+   XOR BX, BX
+   MOV BL, indexOperaciones
+   DEC BX
+   MOV pilaOperaciones[BX], AX
+   poppear
+ENDM
+
+obtenerUltimoPila MACRO
+   pushear
+   XOR BX, BX
+   XOR AX, AX
+   MOV BL, indexOperaciones
+   DEC BX
+   MOV AX, pilaOperaciones[BX]
+   MOV operador1, AX
+   poppear
+ENDM
+
+transformarNumeroAlmacenarPila MACRO
+   pushear
+   ;=============SI contiene el numero mas significativo
+   ;=============SI + 1 contiene el numero menos significativo
+   ;=============Los numeros se reciben en ascii. 0 -> 0030H, 1 -> 0031H, ... , 9 -> 0039H
+   SUB AL, 0030H ; Nos quedamos con el valor en hexa
+   SUB AH, 0030H ; Nos quedamos con el valor en hexa
+
+   MOV numeroMas, AL ; Guardamos los datos de los numeros
+   MOV numeroMenos, AH ; Guardamos los datos de los numeros
+
+   MOV BL, numeroMas
+   MOV AL, 0010 ; Para operar las decenas                AB          A = 5, B = 3
+   MUL BL ; Obtenemos las decenas                        A*10        5*10 = 50
+   ADD AL, numeroMenos ; Sumamos las unidades            A*10 + B    50 + 3 = 53
+
+   ;============AL contiene el resultado de la conversion de los numeros
+   XOR BX, BX
+   MOV BL, indexOperaciones
+   MOV SI, BX
+   XOR AH, AH
+   MOV pilaOperaciones[SI], AX
+   INC indexOperaciones
+   poppear
+ENDM
+
+operarPila MACRO
+   LOCAL CICLO, SUMA, RESTA, CONTINUAR, SALIR, OVER
+   pushear
+   XOR BX, BX
+   MOV AX, pilaOperaciones[BX]
+   MOV AH, 0000
+   MOV resultado, AX
+   CICLO:
+      MOV AX, pilaOperaciones[BX]
+      MOV CX, pilaOperaciones[BX + 2]
+      MOV DX, pilaOperaciones[BX + 1]
+      CMP DL, 002BH ; Es suma
+      JE SUMA
+      CMP DL, 002DH ; Es suma
+      JE RESTA
+      JMP SALIR
+
+      SUMA:
+         ;print essuma
+         ;MOV operador1, CX
+         ;convertirNumero resultado
+         ;print mostrarRespuesta
+         ;CALL PAUSA
+
+         CALL PROBAROVERF
+         ADD resultado, CX ; Se guarda el resultado
+         ADD BX, 0002
+         ;MOV pilaOperaciones[BX], AX
+         JMP CONTINUAR
+   
+      RESTA:
+         ;print esresta
+         CALL PROBAROVERF
+         SUB resultado, CX ; Se guarda el resultado
+         ADD BX, 0002
+         ;MOV pilaOperaciones[BX], AX
+         JMP CONTINUAR
+
+      CONTINUAR:
+         MOV DX, pilaOperaciones[BX + 1]
+         CMP DL, '$' ; Fin de cadena
+         JE SALIR
+         JMP CICLO
+         
+
+   SALIR:
+      poppear
+ENDM
+
+convertirNumero MACRO r
+   ; resultadoOperacion tiene el resultado del archivo
+   pushear
+   MOV DX, 0000
+   MOV AX, r
+   MOV BX, 2710H
+   DIV BX
+   MOV mostrarRespuesta[0000], AL
+   ADD mostrarRespuesta[0000], 0030H
+
+   MOV AX, DX
+   XOR DX, DX
+   MOV BX, 03E8H
+   DIV BX
+   MOV mostrarRespuesta[0001], AL
+   ADD mostrarRespuesta[0001], 0030H
+
+   MOV AX, DX
+   XOR DX, DX
+   MOV BX, 0064H
+   DIV BX
+   MOV mostrarRespuesta[0002], AL
+   ADD mostrarRespuesta[0002], 0030H
+
+   MOV AX, DX
+   XOR DX, DX
+   MOV BX, 000AH
+   DIV BX
+   MOV mostrarRespuesta[0003], AL
+   ADD mostrarRespuesta[0003], 0030H
+
+   MOV AX, DX
+   XOR DX, DX
+   MOV BX, 0001H
+   DIV BX
+   MOV mostrarRespuesta[0004], AL
+   ADD mostrarRespuesta[0004], 0030H
+   poppear
+ENDM
